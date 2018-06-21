0ab98e377f6353fed92758d74bfa2c176d2442d5
marco238 <marco238cuba@gmail.com>
2018-06-21 20:49:53 +0200

    feat: navbar and section components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PlanCardsComponent } from './components/plan-cards/plan-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PlanCardsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/plan-cards/plan-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-cards',
+  templateUrl: './plan-cards.component.html',
+  styleUrls: ['./plan-cards.component.css']
+})
+export class PlanCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
