e1b363ca1a0bf7e86418dda11543d4f4b363e434
marco238 <marco238cuba@gmail.com>
2018-06-22 15:36:14 +0200

    feat: cards highlighted when selected

--- src/app/components/plan-cards/plan-cards.component.ts
+++ src/app/components/plan-cards/plan-cards.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-cards.component.css']
 })
 export class PlanCardsComponent implements OnInit {
+  card1: any;
+  card2: any;
+  card3: any;
+  card4: any;
+  card5: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.card1 = document.querySelector('.c1 input');
+    this.card2 = document.querySelector('.c2 input');
+    this.card3 = document.querySelector('.c3 input');
+    this.card4 = document.querySelector('.c4 input');
+    this.card5 = document.querySelector('.c5 input');
+  }
+
+  refresh() {
+    this.card1 = document.querySelector('.c1 input');
+    this.card2 = document.querySelector('.c2 input');
+    this.card3 = document.querySelector('.c3 input');
+    this.card4 = document.querySelector('.c4 input');
+    this.card5 = document.querySelector('.c5 input');
   }
 
 }
